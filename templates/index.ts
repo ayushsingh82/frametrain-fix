10f4b6628f0c8dff011d35a5dd906d41c4acfaf0
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-06-17 15:25:01 +0530

    eyesoothing fixes

---
+++ templates/index.ts
@@ -0,0 +1,15 @@
+import discourse from './discourse'
+// import basic from './basic'
+import pdf from './pdf'
+import poll from './poll'
+import sablier from './sablier'
+import twitter from './twitter'
+
+export default {
+    // basic,
+    pdf,
+    poll,
+    sablier,
+    twitter,
+    discourse,
+}
